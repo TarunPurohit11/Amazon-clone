bb653caaa71a37fb6c5213496e0d8728e6478132
Tarun <prht.tarun@gmail.com>
2025-09-10 07:01:15 +0530

    started building order page

--- data/products.js
+++ data/products.js
@@ -48,14 +48,13 @@ export function loadProductsfetch(){
     console.log(response);
     return response.json();
   }).then((productsData) => {
-    console.log(productsData);
+    
     products = productsData.map((productDetails) => {
       if(productDetails.type === 'clothing'){
         return new Clothing(productDetails);
       }  
       return new Product(productDetails);
     });
-    console.log('fetch products');
   }).catch((error) =>{
     console.log('Unexpected error');
   });
